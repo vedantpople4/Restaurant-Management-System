8e70fe7840b266a22425572b59c10e5296983336
Vedant pople <vedantpople4@gmail.com>
2022-03-09 23:32:17 +0530

    Routes done

---
+++ controllers/foodController.go
@@ -0,0 +1,35 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.context) {
+
+	}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.context) {
+
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.context) {
+
+	}
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.context) {
+
+	}
+}
+
+func round(num float64) int {
+
+}
+
+func toFixed(num float64) int {
+
+}
